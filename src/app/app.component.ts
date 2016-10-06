e8e671c48186557aa2775533e118ec0acc49b4b1
cmermingas <cmermingas@gmail.com>
2016-10-06 12:28:58 -0400

    Web app capable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, style, state, animate, transition, trigger} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('flyIn', [
+      state('in', style({transform: 'translateX(0)'})),
+      transition('void => *', [
+        style({transform: 'translateX(100%)'}),
+        animate(100)
+      ])
+    ])
+  ]
+
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Angular Nav Tabs';
+  activeTab = 1;
+
+  clickTab(tabNumber) {
+    this.activeTab = tabNumber;
+  }
 }
